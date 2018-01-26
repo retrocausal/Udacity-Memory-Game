dfe26feed51ea8c769d5035b0ba47db13a25c62a
Nitin Prabhakar <pmitwithnitin@gmail.com>
2018-01-26 23:17:25 +0530

    Pull all webcomponent imports into one http request

--- scripts/ready.js
+++ scripts/ready.js
@@ -1,11 +1,19 @@
-$(() => {
+window.addEventListener('WebComponentsReady', (W) => {
   const superHeroMindMap = games.oSuperheroMindMap();
   const play = function () {
     superHeroMindMap.build();
     superHeroMindMap.layout(".deck");
     return superHeroMindMap.activate();
   };
-  window.addEventListener('WebComponentsReady', (W) => {
-    return play();
-  });
+  const docLoaded = () => {
+    let check;
+    if (document.readyState === "complete") {
+      setTimeout(play, 100);
+      return check = window.cancelAnimationFrame(check);
+    }
+    return check = window.requestAnimationFrame(() => {
+      return docLoaded();
+    });
+  };
+  return docLoaded();
 });
